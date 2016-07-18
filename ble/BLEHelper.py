f0198f728c29db14073938b62777573767852ed3
apache@tencent.com <apache@tencent.com>
2016-07-18 17:08:19 +0800

    update

--- ble/BLEHelper.py
+++ ble/BLEHelper.py
@@ -1,14 +1,28 @@
 #-*- coding: utf-8 -*-
 from bluepy.btle import Scanner
 from ScanDelegate import ScanDelegate
+import threading
 
 class BLEHelper(object):
     
     def __init__(self):
+        self.scanThread = None
         self.scanner = Scanner().withDelegate(ScanDelegate())
+    
+    def __scanInThread(self, timeout):
+        self.scanThread = threading.Thread(target=self.scanner.scan, args=(timeout,))
+        self.scanThread.start()
+        return True
         
     def startScan(self, timeout=10):
-        self.scanner.scan(timeout)
+        if self.scanThread is None:
+            return self.__scanInThread(timeout)
+        else:
+            if self.scanThread.is_alive():
+                return False
+            else:
+                self.scanThread = None
+                return self.startScan(timeout)
         
     def stopScan(self):
         self.scanner.stop()
